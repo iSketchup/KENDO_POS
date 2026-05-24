99a0b2997ee6fe72b3d29e21588091ac2950db5e
Daniel WALSER <daniel.walser@student.htl-rankweil.at>
2026-05-24 19:17:20 +0200

    ... Anpassung

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using Main.ViewModels;
+
+
+Console.WriteLine("Hello, World!");
+
+var result1 = await Userhandling.VaidateLogin("Daniel", "123HTL12");
+if (result1 != null)
+{
+    Console.WriteLine("Login erfolgreich");
+}
+
+var result2 = await Userhandling.VaidateLogin("Walser", "DanHTLWal");
+if (result2 != null)
+{
+    Console.WriteLine("Login erfolgreich");
+} else
+{
+    Console.WriteLine("Fehlgeschlagen");
+}
