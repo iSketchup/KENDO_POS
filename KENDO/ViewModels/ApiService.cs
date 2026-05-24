9a97bd9463e2034120c1cc5e74469d9c9210e003
Daniel WALSER <daniel.walser@student.htl-rankweil.at>
2026-05-24 16:25:15 +0200

    Hier ist die API-Klasse.

---
+++ KENDO/ViewModels/ApiService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Main.Models;
+using System.Text;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+using System.Threading.Tasks;
+
+namespace Main.ViewModels;
+
+public class ApiService
+{
+    private HttpClient _client;
+
+    public ApiService(HttpClient client)
+    {
+        _client = client;
+    }
+    
+    public async Task<List<User>> GetUserInfo()
+    {
+        List<User>? response = await _client.GetFromJsonAsync<List<User>>("user");
+
+        // Neue Liste wird erzeugt, falls der User nicht geholt werden kann.
+        return response ?? new List<User>();
+    }
+
+    public async Task CreateUser(User user)
+    {
+        // Bevor man etwas einfügt, wird auf eine Antwort der API gewartet
+        HttpResponseMessage result = await _client.PostAsJsonAsync("user", user);
+        result.EnsureSuccessStatusCode();
+    }
+}
