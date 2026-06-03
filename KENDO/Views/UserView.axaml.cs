42e6de6cb750e5a92739a529525fccecab1de554
iSketch_up <kalulu@gmx.at>
2026-06-03 10:35:50 +0200

    init user view

---
+++ KENDO/Views/UserView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Main.Views;
+
+public partial class UserView : UserControl
+{
+    public UserView()
+    {
+        InitializeComponent();
+    }
+}
